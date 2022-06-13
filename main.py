3c22229ad0d8ca13b06ac8867ea45869160edcfe
JaturongSarak <93725807+JaturongSarak@users.noreply.github.com>
2022-06-13 16:23:48 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ class BMICalculator:
     def bmi(self):
         return self.weight / ((self.height / 100) ** 2)
 
-    def chart(self):
-        bmi = self.bmi()
-        if bmi >= 30:
-            return 'Obese'
-        elif 30 > bmi >= 25:
-            return 'Overweight'
-        elif 25 > bmi >= 18.5:
-            return 'Healthy'
-        else:
+    def chart(self, bmi):
+        if bmi < 18.5:
             return 'Underweight'
+        elif bmi < 25:
+            return 'Healthy'
+        elif bmi < 30:
+            return 'Overweight'
+        else:
+            return 'Obese'
+
 
 print('BMI Calculator')
 weight = input('Weight (kg.) : ')
 height = input('Height (cm.) : ')
 Robot = BMICalculator(float(weight), float(height))
-print(Robot.bmi())
-print(Robot.chart())
+bmi = Robot.bmi()
+chart = Robot.chart(bmi)
+print(bmi)
+print(chart)
